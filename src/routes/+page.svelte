3343384a28df0c5db3e2f3096368955aafd26529
Colin Kautz <colin.kautz@gmail.com>
2025-01-13 22:54:43 -0600

    adding the basics for now

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Title from "$lib/components/Title.svelte";
+    import Header from "$lib/components/Header.svelte";
+    import LeaderBoard from "$lib/components/Home/LeaderBoard.svelte";
+</script>
+
+<Title text="Welcome to TreeShaker"/>
+
+<Header text="Tree Shaker" isCentered={true}/>
+<LeaderBoard/>
