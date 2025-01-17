ca61fff2f30d37f01f65a3f920199f32530ea6dd
Colin Kautz <colin.kautz@gmail.com>
2025-01-16 22:31:43 -0600

    getting that tree page going

---
+++ src/routes/tree/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+    import Title from "$lib/components/Title.svelte";
+    import Header from "$lib/components/Header.svelte";
+    import "$lib/styles/tree.css";
+
+    let numberOfTurns = 9;
+    const turnLabel = numberOfTurns > 1 ? "turns" : "turn";
+    const hasTurnsLeft = numberOfTurns > 0;
+</script>
+
+<Title text="Shake the tree!"/>
+
+{#if hasTurnsLeft}
+    <Header text="You have {numberOfTurns} {turnLabel} left, click the tree" isCentered={true}/>
+{:else}
+    <Header text="Oh no, you are out of turns!" isCentered={true}/>
+{/if}
+
+<main>
+    <img class="the-tree {hasTurnsLeft ? 'clickable' : ''}" src="/the-tree.png" alt="a tree"/>
+</main>
+
+<style>
+    .the-tree {
+        position: relative;
+        top: 125px;
+        cursor: not-allowed;
+    }
+
+    .the-tree.clickable {
+        cursor: pointer;
+    }
+
+    main {
+        display: flex;
+        justify-content: center;
+    }
+</style>
