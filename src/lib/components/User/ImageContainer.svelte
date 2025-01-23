009b95b5a53c3df530f63ac48e3b476f0c378cd3
Colin Kautz <colin.kautz@gmail.com>
2025-01-22 21:51:20 -0600

    just stylin things

--- src/lib/components/User/ImageContainer.svelte
+++ src/lib/components/User/ImageContainer.svelte
@@ -1,15 +1,38 @@
 <script>
-    let {produceType} = $props();
+    let {imageGroup, category} = $props();
+    const formattedCategory = category.split(/(?=[A-Z])/).join(" ");
 </script>
 
-<div>
-    {#each produceType as produce}
-        <img class="produce-image" src={produce.url} alt={produce.name} />
+<div class="produce-image-container">
+    <h3>{formattedCategory}</h3>
+    {#each imageGroup as image}
+        <img class="produce-image unobtained" src={image.url} alt={image.name} />
     {/each}
 </div>
 
 <style>
-    .produce-image {
-        height: 50px;
+    .unobtained {
+        filter: brightness(0);
+        opacity: 75%;
+    }
+
+    .produce-image-container {
+        border: 4px solid var(--color-deep-forest);
+        border-radius: 15px;
+        margin: 10px 0;
+        padding: 0 10px;
+
+        .produce-image {
+            height: 45px;
+            margin: 0 5px;
+        }
+
+        .produce-image:first-of-type {
+            margin-left: 0;
+        }
+
+        .produce-image:last-of-type {
+            margin-right: 0;
+        }
     }
 </style>
