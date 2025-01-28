0e801cc9a711db14156a1a469e67f342dd8e9cea
Colin Kautz <colin.kautz@gmail.com>
2025-01-27 22:15:13 -0600

    cleaning up styles and text

--- src/lib/components/Home/LoginButton.svelte
+++ src/lib/components/Home/LoginButton.svelte
@@ -1,24 +1,21 @@
 <div class="login-container">
-    <a class="login-button" href="/tree">Login</a>
+    <a class="login-button" href="/tree">Login with Twitch</a>
 </div>
 
 <style>
     .login-button {
         display: block;
         text-decoration: none;
-        background-color: var(--color-rich-walnut);
+        background-color: var(--color-deep-forest);
         color: var(--color-white);
         border: none;
         border-radius: 4px;
         width: 250px;
+        height: 40px;
         margin: 20px auto 0;
-        padding: 10px 0;
+        padding: 4px 0;
+        font-size: 20px;
         cursor: pointer;
-        transition: background-color 0.3s ease;
-    }
-
-    .login-button:hover {
-        background-color: var(--color-golden-acorn);
     }
     
     .login-container {
