36a95f1250c797177a7fec02ad6b40832edfc261
Colin Kautz <colin.kautz@gmail.com>
2025-02-06 23:05:52 -0600

    fixing the header stuff

--- src/lib/components/Tree/HeaderWithLink.svelte
+++ src/lib/components/Tree/HeaderWithLink.svelte
@@ -1,12 +1,16 @@
 <script>
-    let {text} = $props();
+    let {text, username} = $props();
 </script>
 
 <header>
-    <h1 class="header-text">{text}</h1>
+    <h1 class="header-text">Hi, <a href="/user/{username}">{username}</a>! {text}</h1>
 </header>
 
 <style>
+    a, a:visited {
+        color: inherit;
+    }
+
     .header-text {
         font-size: 40px;
         font-weight: normal;
