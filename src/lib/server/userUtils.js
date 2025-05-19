cf200287131c5e38e7c7b0d32bcb32503b099be0
Colin Kautz <colin.kautz@gmail.com>
2025-05-18 21:34:26 -0500

    more descriptive console errors

--- src/lib/server/userUtils.js
+++ src/lib/server/userUtils.js
@@ -4,7 +4,7 @@ export async function getObtainedProduceData(username) {
     const {data, error} = await supabase.from("obtained_produce").select(`produce_name, quantity`).eq("name", username);
 
     if (error) {
-        console.error("Error fetching user data:", error);
+        console.error("Error fetching getObtainedProduceData:", error);
         return [];
     }
     return data || [];
@@ -14,7 +14,7 @@ export async function getUserBalance(username) {
     const {data, error} = await supabase.from("users").select(`balance`).eq("name", username);
 
     if (error) {
-        console.error("Error fetching user data:", error);
+        console.error("Error fetching getUserBalance:", error);
         return [];
     }
     return data || [];
@@ -24,7 +24,7 @@ export async function getUserData(username) {
     const { data, error } = await supabase.from("users").select().eq("name", username);
 
     if (error) {
-        console.error("Error fetching user data:", error);
+        console.error("Error fetching getUserData:", error);
         return [];
     }
     return data || [];
