a4797447137e5169a08f985cd6f80792b842a6af
Colin Kautz <colin.kautz@gmail.com>
2025-05-18 20:00:23 -0500

    forgot to remove a drizzle file whoops

--- drizzle.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    dialect: "postgresql",
-    schema: "./src/lib/server/db/schema.js",
-    out: ".drizzle",
-    dbCredentials: {
-        url: process.env.POSTGRES_URL
-    }
-}
